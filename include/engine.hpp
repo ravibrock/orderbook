c70a6924ca7b06dba83dc4a391e9b59b18f37ced
ravibrock <66334356+ravibrock@users.noreply.github.com>
2025-03-17 21:20:09 -0500

    Add initial engine interface

---
+++ include/engine.hpp
@@ -0,0 +1,27 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include <unordered_map>
+#include <vector>
+#include "orderbook.hpp"
+
+struct Order {
+    std::string user;
+    std::string direction;
+    std::string asset;
+    int quantity;
+    int price;
+};
+
+class Engine {
+    public:
+        Engine();
+        void add_orderbook(std::string asset);
+        bool orderbook_exists(std::string asset);
+        std::vector<Order> place_order(Order order);
+
+    private:
+        std::unordered_map<std::string, Orderbook> orderbooks;
+};
+
+#endif // ENGINE_H
